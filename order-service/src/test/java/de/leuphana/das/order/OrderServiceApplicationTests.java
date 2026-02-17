b47cfaf8994c01b7a5b571fa8303b6551735accc
Mia Mainka <Mia.Mainka@gmail.com>
2026-02-17 22:29:24 +0100

    Setting up springboot projects

---
+++ order-service/src/test/java/de/leuphana/das/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.leuphana.das.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
